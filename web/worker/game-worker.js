3dd00050845471ed2527eb755d7715587be081b4
i10veU <166589198+i10veU@users.noreply.github.com>
2026-09-23 20:16:21 +0900

    Phase 271: run causal case simulation in worker

--- web/worker/game-worker.js
+++ web/worker/game-worker.js
@@ -1,6 +1,6 @@
 self.AnonymousRPG = self.AnonymousRPG || {};
 const window = self;
-importScripts("../core/game-state.js", "../data/npcs.js", "../data/places.js", "../data/cases.js", "../core/economy-world.js", "../core/regional-economy.js", "../core/faction-world.js", "../core/organization-world.js", "../core/npc-goals.js", "../core/npc-relations.js", "../core/action-resolver.js", "../core/npc-simulation.js");
+importScripts("../core/game-state.js", "../data/npcs.js", "../data/places.js", "../data/cases.js", "../core/economy-world.js", "../core/regional-economy.js", "../core/faction-world.js", "../core/organization-world.js", "../core/npc-goals.js", "../core/npc-relations.js", "../core/action-resolver.js", "../core/npc-simulation.js", "../core/case-causality.js");
 
 let state = null;
 
@@ -19,6 +19,7 @@ function runAction(text) {
   const npcRelationEvents = AnonymousRPG.Core.simulateNPCRelations ? AnonymousRPG.Core.simulateNPCRelations(state, after) : [];
   const economyEvent = AnonymousRPG.Core.simulateEconomy(state, after);
   const regionalEconomyEvent = AnonymousRPG.Core.simulateRegionalEconomy ? AnonymousRPG.Core.simulateRegionalEconomy(state, after) : null;
+  const caseCausalityEvent = AnonymousRPG.Core.simulateCaseCausality ? AnonymousRPG.Core.simulateCaseCausality(state, after) : null;
 
   if (result.narrative) AnonymousRPG.Core.appendLog(state, result.narrative, result.action);
   npcEvents.forEach(function (event) {
@@ -35,6 +36,7 @@ function runAction(text) {
   });
   if (economyEvent) AnonymousRPG.Core.appendLog(state, economyEvent, null, true);
   if (regionalEconomyEvent) AnonymousRPG.Core.appendLog(state, regionalEconomyEvent, null, true);
+  if (caseCausalityEvent) AnonymousRPG.Core.appendLog(state, caseCausalityEvent, null, true);
   return { result: result, npcEvents: npcEvents, economyEvent: economyEvent };
 }
 
@@ -53,6 +55,7 @@ self.onmessage = function (event) {
       if (AnonymousRPG.Core.ensureOrganizationRelations) AnonymousRPG.Core.ensureOrganizationRelations(state);
       if (AnonymousRPG.Core.ensureNPCRelations) AnonymousRPG.Core.ensureNPCRelations(state);
       if (AnonymousRPG.Core.ensureNPCGoals) AnonymousRPG.Core.ensureNPCGoals(state);
+      if (AnonymousRPG.Core.ensureCaseCausality) AnonymousRPG.Core.ensureCaseCausality(state);
       AnonymousRPG.Data.ensureCases(state);
       reply("READY", { state: state });
       return;
@@ -76,6 +79,7 @@ self.onmessage = function (event) {
       AnonymousRPG.Core.ensureOrganizations(state);
       if (AnonymousRPG.Core.ensureOrganizationRelations) AnonymousRPG.Core.ensureOrganizationRelations(state);
       if (AnonymousRPG.Core.ensureNPCGoals) AnonymousRPG.Core.ensureNPCGoals(state);
+      if (AnonymousRPG.Core.ensureCaseCausality) AnonymousRPG.Core.ensureCaseCausality(state);
       AnonymousRPG.Data.ensureCases(state);
       AnonymousRPG.Core.appendLog(state, "비가 그친 새벽이다. 젖은 돌바닥 위로 사람들이 하루를 시작했다. 누구도 당신을 기다리지 않는다.");
       AnonymousRPG.Core.appendLog(state, "북문 시장에서는 가게마다 곡물 가격이 조금씩 다르다. 광장 건너편에서는 경비대원이 상인의 저울을 확인하고 있다.");
